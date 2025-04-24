20e23354c00db787d9f1692a381a1daf3ef94625
JAVARCE-1 <jaw.arce@gmail.com>
2025-04-23 22:23:32 -0500

    añadir clase SistemaAlquiler e interfaces

---
+++ AlquilerVehiculos_POO/Modelos/SistemaAlquiler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerVehiculos_POO.Modelos
+{
+    class SistemaAlquiler
+    {
+        private List<Vehiculo> _vehiculo = new List<Vehiculo>();
+        private List<Cliente> _cliente = new List<Cliente>();
+
+
+        public void AgregarCliente(Cliente cliente)
+        {
+            _cliente.Add(cliente);
+        }
+
+        public void AgregarVehiculo(Vehiculo vehiculo)
+        {
+            _vehiculo.Add(vehiculo);
+        }
+
+        public List<Vehiculo> ObtenerVehiculos()
+        {
+            return _vehiculo.ToList();
+        }
+
+        public List<Cliente> ObtenerCliente()
+        {
+            return _cliente.ToList();
+        }
+    }
+}
--- AlquilerVehiculos_POO/Program.cs
+++ AlquilerVehiculos_POO/Program.cs
@@ -5,6 +5,52 @@
         static void Main(string[] args)
         {
             Console.WriteLine("SISTEMA ALQUILER DE VEHICULO");
+
+            bool salir = false;
+
+            while (!salir)
+            {
+                Console.WriteLine("\nMenu Principal:");
+                Console.WriteLine("1. Gestion de Vehiculo");
+                Console.WriteLine("2. Gestion de Cliente");
+                Console.WriteLine("3. Alquiler Vehiculo");
+                //Console.WriteLine("4. Reportes");
+                Console.WriteLine("0. Salir");
+
+                Console.WriteLine("\n Seleccione una opcion: ");
+
+                try
+                {
+                    int opcion = int.Parse(Console.ReadLine());
+                    // Entra aqui -> 
+                    switch (opcion)
+                    {
+                        case 1:
+                            //GestionEmpleados(sistema);
+                            break;
+                        case 2:
+                            //GestionProductos(sistema);
+                            break;
+                        case 3:
+                            // MovimientosStock(sistema);
+                            break;
+                        case 4:
+                            // MostrarReportes(sistema);
+                            break;
+                        case 0:
+                            salir = true;
+                            break;
+                        default:
+                            Console.WriteLine("Opcion invalida.");
+                            break;
+                    }
+                }
+                catch (Exception ex) // throw an error
+                {
+                    Console.WriteLine(ex.Message); // IndexOutOfRangeException // En internet
+                }
+            }
+
         }
     }
 }
